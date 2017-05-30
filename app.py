cb3a1226d522137baba143adfe9c882f031288e4
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-30 16:19:32 -0700

    Redo

--- app.py
+++ app.py
@@ -15,54 +15,96 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+# Flask app should start in global layout
 app = Flask(__name__)
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
-	print("Request:")
-	print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-	# Recent Changes (start)
+    res = processRequest(req)
 
-	# # Dictionary Object
-	# res = process_request(req)
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
-	# # Dictionary converted to json
-	# res = json.dumps(rest, indent=4)
-	if True:
-		res = {}
-		res['speech'] = 'this is working'
-		res['displayText'] = 'This is working.'
-		res['source'] = 'pw-assistant-test'
-		return res
-	# Recent Changes (end)
-	
 
-	r = make_response(res)
-	r.headers['Content-Type'] = 'application/json'
-	return r
-
-def process_request(req):
-	if req.get("result").get("action") != "TodaysOpportunities.Direct":
-		return {}
-
-	#Type of opportunity
-	oppType = result.get("result").get("parameters").get("opp-type") 
-
-	speech = "The following people qualify for " + oppType + ": John Doe, Joe Schmo, Lisa Grahm"
-
-	return {
-		"speech": speech,
-		"displayText": speech,
-		"source": "pw-assistant-test" 
-	}
-	## Converting website url to json usable json.
+def processRequest(req):
+    if req.get("result").get("action") != "TodaysOpportunities.Direct":
+        return {}
+    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    # yql_query = makeYqlQuery(req)
+    # if yql_query is None:
+        # return {}
+    # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    oppType = result.get("result").get("parameters").get("opp-type")
     # result = urlopen(yql_url).read()
     # data = json.loads(result)
-    # res = makeWebhookResult(data)
-    # return res
+    speech = "Opportunity is: " + oppType + " -- This is working."
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+# def makeYqlQuery(req):
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     city = parameters.get("geo-city")
+#     if city is None:
+#         return None
+
+#     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+# def makeWebhookResult(data):
+#     query = data.get('query')
+#     if query is None:
+#         return {}
+
+#     result = query.get('results')
+#     if result is None:
+#         return {}
+
+#     channel = result.get('channel')
+#     if channel is None:
+#         return {}
+
+#     item = channel.get('item')
+#     location = channel.get('location')
+#     units = channel.get('units')
+#     if (location is None) or (item is None) or (units is None):
+#         return {}
+
+#     condition = item.get('condition')
+#     if condition is None:
+#         return {}
+
+#     # print(json.dumps(item, indent=4))
+
+#     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+#              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+#     print("Response:")
+#     print(speech)
+
+#     return {
+#         "speech": speech,
+#         "displayText": speech,
+#         # "data": data,
+#         # "contextOut": [],
+#         "source": "apiai-weather-webhook-sample"
+#     }
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
