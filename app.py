83d420623095187f69a004e5fc356a69dd24030b
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-30 11:04:58 -0700

    fixed bug

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from future.standard_library import install_aliases
 install_aliases()
 
 from urllib.parse import urlparse, urlencode
-from urllib.requst import urlopen, Requst
+from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 
 import json
