520ad49e282f41d3ba600bf6a02ac16078109e8b
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-30 10:53:02 -0700

    yahoo weather app test

--- app.py
+++ app.py
@@ -1,62 +1,107 @@
-from __future__ import print_function
-from future.stanard_library import install_aliasas
-install_aliasas()
+#!/usr/bin/env python
 
-# from urllib.parse import urlparse, urlencode
-from urllib.requst import urlopen, Requst
-# from urllib.error import HTTPError
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
 import json
 import os
-import sys
 
 from flask import Flask
 from flask import request
 from flask import make_response
 
+# Flask app should start in global layout
 app = Flask(__name__)
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
-	print("Request:")
-	print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-	# Dictionary Object
-	res = process_request(req)
+    res = processRequest(req)
 
-	# Dictionary converted to json
-	res = json.dumps(rest, indent=4)
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
-	r = make_response(res)
-	r.headers['Content-Type'] = 'application/json'
-	return r
 
-def process_request(req):
-	if req.get("result").get("action") != "TodaysOpportunities.Direct":
-		return {}
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
 
-	#Type of opportunity
-	oppType = result.get("result").get("parameters").get("opp-type") 
 
-	speech = "The following people qualify for " + oppType + ": John Doe, Joe Schmo, Lisa Grahm"
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
 
-	return {
-		"speech": speech,
-		"displayText": speech,
-		#Change this
-		"source": "apiai-weather-webhook-sample" 
-	}
-	## Converting website url to json usable json.
-    # result = urlopen(yql_url).read()
-    # data = json.loads(result)
-    # res = makeWebhookResult(data)
-    # return res
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
 
 if __name__ == '__main__':
-	port = int(os.getenv('PORT'), 5000)
+    port = int(os.getenv('PORT', 5000))
 
-	print("Starting app on port %d" % port)
+    print("Starting app on port %d" % port)
 
-	app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
